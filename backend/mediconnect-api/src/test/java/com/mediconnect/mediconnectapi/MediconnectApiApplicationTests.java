9442829dd7308940ef249cc4f5c903c38631feab
Abdulrahim26 <abdelraheemyahya20@gmail.com>
2026-07-14 16:26:42 +0000

    Create MediConnect database entity model

---
+++ backend/mediconnect-api/src/test/java/com/mediconnect/mediconnectapi/MediconnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediconnect.mediconnectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediconnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
