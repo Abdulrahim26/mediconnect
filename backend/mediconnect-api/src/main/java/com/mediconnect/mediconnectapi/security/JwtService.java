add5b5b745f3381a679e3f2272a10544b1cb5da7
Abdulrahim26 <abdelraheemyahya20@gmail.com>
2026-07-15 23:12:45 +0000

    Implement JWT service

--- backend/mediconnect-api/src/main/java/com/mediconnect/mediconnectapi/security/JwtService.java
+++ backend/mediconnect-api/src/main/java/com/mediconnect/mediconnectapi/security/JwtService.java
@@ -1,4 +1,108 @@
 package com.mediconnect.mediconnectapi.security;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+@Service
 public class JwtService {
+
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+
+    private SecretKey getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                secret.getBytes()
+        );
+    }
+
+
+    public String generateToken(String email, String role) {
+
+
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("role", role);
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + jwtExpiration
+                        )
+                )
+                .signWith(
+                        getSigningKey(),
+                        SignatureAlgorithm.HS256
+                )
+                .compact();
+    }
+
+
+    public String extractEmail(String token) {
+
+        return extractClaim(
+                token,
+                Claims::getSubject
+        );
+    }
+
+
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> resolver
+    ) {
+
+        final Claims claims =
+                Jwts.parserBuilder()
+                        .setSigningKey(getSigningKey())
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+
+        return resolver.apply(claims);
+    }
+
+
+    public boolean isTokenValid(String token) {
+
+        try {
+
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
 }
